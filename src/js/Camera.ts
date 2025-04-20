adb1b68469f09a7afeb4f7b8ed2d172ddbec2bd5
Shlomi Nugarker <shlomin1231@gmail.com>
2025-04-20 20:21:48 +0300

    Refactor Camera class by migrating from JavaScript to TypeScript, updating import statements in Level.js to reflect the change.

---
+++ src/js/Camera.ts
@@ -0,0 +1,16 @@
+import { Vec2 } from "./math.ts";
+
+export default class Camera {
+  public pos: Vec2;
+  public size: Vec2;
+  public min: Vec2;
+  public max: Vec2;
+
+  constructor() {
+    this.pos = new Vec2(0, 0);
+    this.size = new Vec2(256, 224);
+
+    this.min = new Vec2(0, 0);
+    this.max = new Vec2(Infinity, Infinity);
+  }
+}
--- src/js/Level.js
+++ src/js/Level.js
@@ -1,77 +1,78 @@
-import Camera from './Camera.js';
-import MusicController from './MusicController.js';
-import EntityCollider from './EntityCollider.js';
-import Scene from './Scene.js';
-import TileCollider from './TileCollider.js';
-import { clamp } from './math.js';
-import { findPlayers } from './player.js';
+import Camera from "./Camera.ts";
+import MusicController from "./MusicController.js";
+import EntityCollider from "./EntityCollider.js";
+import Scene from "./Scene.js";
+import TileCollider from "./TileCollider.js";
+import { clamp } from "./math.js";
+import { findPlayers } from "./player.js";
 
 function focusPlayer(level) {
-    for (const player of findPlayers(level.entities)) {
-        level.camera.pos.x = clamp(
-            player.pos.x - 100,
-            level.camera.min.x,
-            level.camera.max.x - level.camera.size.x);
-    }
+  for (const player of findPlayers(level.entities)) {
+    level.camera.pos.x = clamp(
+      player.pos.x - 100,
+      level.camera.min.x,
+      level.camera.max.x - level.camera.size.x
+    );
+  }
 }
 
 class EntityCollection extends Set {
-    get(id) {
-        for (const entity of this) {
-            if (entity.id === id) {
-                return entity;
-            }
-        }
+  get(id) {
+    for (const entity of this) {
+      if (entity.id === id) {
+        return entity;
+      }
     }
+  }
 }
 
 export default class Level extends Scene {
-    static EVENT_TRIGGER = Symbol('trigger');
-    static EVENT_COMPLETE = Symbol('complete');
+  static EVENT_TRIGGER = Symbol("trigger");
+  static EVENT_COMPLETE = Symbol("complete");
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.name = "";
+    this.name = "";
 
-        this.checkpoints = [];
+    this.checkpoints = [];
 
-        this.gravity = 1500;
-        this.totalTime = 0;
+    this.gravity = 1500;
+    this.totalTime = 0;
 
-        this.camera = new Camera();
+    this.camera = new Camera();
 
-        this.music = new MusicController();
+    this.music = new MusicController();
 
-        this.entities = new EntityCollection();
+    this.entities = new EntityCollection();
 
-        this.entityCollider = new EntityCollider(this.entities);
-        this.tileCollider = new TileCollider();
-    }
+    this.entityCollider = new EntityCollider(this.entities);
+    this.tileCollider = new TileCollider();
+  }
 
-    draw(gameContext) {
-        this.comp.draw(gameContext.videoContext, this.camera);
-    }
+  draw(gameContext) {
+    this.comp.draw(gameContext.videoContext, this.camera);
+  }
 
-    update(gameContext) {
-        this.entities.forEach(entity => {
-            entity.update(gameContext, this);
-        });
+  update(gameContext) {
+    this.entities.forEach((entity) => {
+      entity.update(gameContext, this);
+    });
 
-        this.entities.forEach(entity => {
-            this.entityCollider.check(entity);
-        });
+    this.entities.forEach((entity) => {
+      this.entityCollider.check(entity);
+    });
 
-        this.entities.forEach(entity => {
-            entity.finalize();
-        });
+    this.entities.forEach((entity) => {
+      entity.finalize();
+    });
 
-        focusPlayer(this);
+    focusPlayer(this);
 
-        this.totalTime += gameContext.deltaTime;
-    }
+    this.totalTime += gameContext.deltaTime;
+  }
 
-    pause() {
-        this.music.pause();
-    }
+  pause() {
+    this.music.pause();
+  }
 }
