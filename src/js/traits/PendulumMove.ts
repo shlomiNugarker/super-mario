a009f0dc6c585d39c593dc45b47572b96e9ed302
Shlomi Nugarker <shlomin1231@gmail.com>
2025-04-21 18:02:29 +0300

    Refactor Goomba and Koopa entities to use TypeScript for PendulumMove trait, updating import statements for consistency and type safety. Add new PendulumMove.ts file to implement pendulum movement behavior.

--- src/js/traits/PendulumMove.ts
+++ src/js/traits/PendulumMove.ts
@@ -1,20 +1,23 @@
-import { Sides } from '../Entity.js';
+import { Sides } from '../Entity.ts';
 import Trait from '../Trait.ts';
 
 export default class PendulumMove extends Trait {
+  private enabled: boolean;
+  private speed: number;
+
   constructor() {
     super();
     this.enabled = true;
     this.speed = -30;
   }
 
-  obstruct(entity, side) {
+  obstruct(entity: any, side: symbol): void {
     if (side === Sides.LEFT || side === Sides.RIGHT) {
       this.speed = -this.speed;
     }
   }
 
-  update(entity) {
+  update(entity: any): void {
     if (this.enabled) {
       entity.vel.x = this.speed;
     }
